927ef52c2464136b226c9b793bd90ba7f1922753
Louis Heal <lh420@ic.ac.uk>
2022-10-17 13:42:13 +0100

    feat: create function that returns a randomly selected node in an attack graph

--- KotlinClient/src/main/kotlin/main.kt
+++ KotlinClient/src/main/kotlin/main.kt
@@ -3,6 +3,7 @@ import org.neo4j.driver.Driver
 import org.neo4j.driver.GraphDatabase
 import org.neo4j.driver.Session
 import org.neo4j.driver.Values.parameters;
+import kotlin.random.Random
 
 val driver: Driver = GraphDatabase.driver("neo4j+s://42ce3f9a.databases.neo4j.io", AuthTokens.basic("neo4j", "qufvn4LK6AiPaRBIWDLPRzFh4wqzgI5x_n2bXHc1d38"))
 
@@ -19,8 +20,24 @@ fun get_graph() {
     println(result)
 }
 
-fun main() {
-    println("Hello, World!")
-    get_graph()
+fun getRandomNode(): org.neo4j.driver.Record {
+    val session: Session = driver.session()
+
+    val maxId: Int = session.writeTransaction { tx ->
+        val result: org.neo4j.driver.Result = tx.run("MATCH(n) RETURN MAX (n.node_id)", parameters())
+        result.list()[0].get(0).toString().toInt()
+    }
+
+    val randInt: Int = Random.nextInt(0,maxId) + 1
+
+    val result: org.neo4j.driver.Record = session.writeTransaction { tx ->
+        val result: org.neo4j.driver.Result = tx.run("MATCH(n {node_id: ${randInt}}) RETURN n", parameters())
+        result.list()[0]
+    }
+    return result
+}
+
+fun main(args: Array<String>) {
+    getRandomNode()
     close()
 }
