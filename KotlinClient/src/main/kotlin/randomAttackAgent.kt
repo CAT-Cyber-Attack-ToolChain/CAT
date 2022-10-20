9d59b444f65963a77a6f851e137b0342c41d5f05
Louis Heal <lh420@ic.ac.uk>
2022-10-20 14:34:51 +0100

    feat: implement function to find the ids of all nodes connected to a given node

--- KotlinClient/src/main/kotlin/randomAttackAgent.kt
+++ KotlinClient/src/main/kotlin/randomAttackAgent.kt
@@ -9,9 +9,8 @@ import kotlin.random.Random
 class randomAttackAgent {
 
     private val driver: Driver = GraphDatabase.driver("neo4j+s://42ce3f9a.databases.neo4j.io", AuthTokens.basic("neo4j", "qufvn4LK6AiPaRBIWDLPRzFh4wqzgI5x_n2bXHc1d38"))
-    private val path: List<String> = listOf()
 
-    private fun getRandomNode(): org.neo4j.driver.Record {
+    private fun getRandomNode(): Int {
         val session: Session = driver.session()
 
         val maxId: Int = session.writeTransaction { tx ->
@@ -19,16 +18,29 @@ class randomAttackAgent {
             result.list()[0].get(0).toString().toInt()
         }
 
-        val randInt: Int = Random.nextInt(0,maxId) + 1
+        return Random.nextInt(0, maxId) + 1
+    }
 
-        val result: org.neo4j.driver.Record = session.writeTransaction { tx ->
-            val result: org.neo4j.driver.Result = tx.run("MATCH(n {node_id: ${randInt}}) RETURN n", parameters())
-            result.list()[0]
+    private fun getConnectedNodes(id: Int): List<Int> {
+        val session: Session = driver.session()
+
+        val connectedNodeIds: List<Int> = session.writeTransaction { tx ->
+            val result: org.neo4j.driver.Result = tx.run("MATCH(start {node_id: ${id}})-[:To]->(end) RETURN (end.node_id)", parameters())
+            result.list().map { e -> e[0].toString().toInt() }
         }
-        return result
+
+        return connectedNodeIds
     }
 
     fun attack() {
-        val startNode: org.neo4j.driver.Record = getRandomNode()
+        val startNodeId: Int = getRandomNode()
     }
+
+}
+
+fun main(args: Array<String>) {
+
+    val randomAttackAgent: randomAttackAgent = randomAttackAgent()
+    randomAttackAgent.attack()
+
 }
