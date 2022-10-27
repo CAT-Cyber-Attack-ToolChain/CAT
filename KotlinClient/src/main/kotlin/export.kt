7515d93d53e3f7c663e4ac5be68559887ebd2327
Anand Doshi <anand.doshi20@imperial.ac.uk>
2022-10-27 14:47:31 +0100

    Started implementing internal representation from the JSON graph from Neo4j.

--- KotlinClient/src/main/kotlin/export.kt
+++ KotlinClient/src/main/kotlin/export.kt
@@ -1,3 +1,5 @@
+import java.io.StringReader
+
 import org.neo4j.driver.AuthTokens
 import org.neo4j.driver.Driver
 import org.neo4j.driver.GraphDatabase
@@ -5,6 +7,11 @@ import org.neo4j.driver.Session
 import org.neo4j.driver.Values.parameters
 import org.neo4j.driver.Result
 import org.neo4j.driver.Record
+
+import com.beust.klaxon.Klaxon
+import com.beust.klaxon.JsonReader
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.JsonArray
 
 class Export {
     companion object {
@@ -14,7 +21,7 @@ class Export {
             driver.close()
         }
 
-        fun exportToJSON() {
+        fun exportToJSON() : String {
             val session: Session = driver.session()
             val result: String = session.writeTransaction { tx ->
                 val result: org.neo4j.driver.Result = tx.run("CALL apoc.export.json.all(null,{useTypes:true,stream: true,jsonFormat:'ARRAY_JSON'})\n" +
@@ -22,11 +29,95 @@ class Export {
                         "RETURN file, nodes, relationships, properties, data", parameters())
                 result.single().get("data").toString()
             }
-            println(result)
+            var trimmed = result.replaceFirst("\"", "")
+            trimmed = trimmed.replaceAfterLast("]", "")
+            trimmed = trimmed.replace("\\", "")
+
+            return trimmed
+
         }
 
         fun translateToCytoscapeJS(json: String) {
-            // TODO: translate json to something cytoscape can use
+
+            val klaxon = Klaxon()
+            JsonReader(StringReader(json)).use { reader ->
+                reader.beginArray() {
+                    while (reader.hasNext()) {
+                        reader.beginObject() {
+                            var type: String = "None"
+                            var id: String = "-1"
+                            var labels: List<String> = arrayListOf<String>()
+                            var properties: JsonObject? = null
+
+                            // relationship
+                            var label: String = "None"
+                            var start: Node? = null
+                            var end: Node? = null
+
+                            while (reader.hasNext()) {
+                                val readName = reader.nextName()
+                                when (readName) {
+                                    "type" -> type = reader.nextString()
+                                    "id" -> id = reader.nextString()
+                                    "labels" -> labels =
+                                        reader.nextArray().map { a -> a.toString() }
+                                    "properties" -> properties = reader.nextObject()
+                                    "label" -> label = reader.nextString()
+                                    "start" -> reader.beginObject() {
+                                        var startId: String = ""
+                                        var startLabels: List<String> = arrayListOf<String>()
+                                        var startProperties: JsonObject? = null
+                                        while (reader.hasNext()) {
+                                            val startName = reader.nextName()
+                                            when (startName) {
+                                                "id" -> startId = reader.nextString()
+                                                "labels" -> startLabels = reader.nextArray().map {a -> a.toString()}
+                                                "properties" -> startProperties = reader.nextObject()
+                                                else -> println("Miss")
+                                            }
+                                        }
+                                        start = Node(startId, startLabels, startProperties)
+                                    }
+                                    "end" -> reader.beginObject() {
+                                        var endId: String = ""
+                                        var endLabels: List<String> = arrayListOf<String>()
+                                        var endProperties: JsonObject? = null
+                                        while (reader.hasNext()) {
+                                            val endName = reader.nextName()
+                                            when (endName) {
+                                                "id" -> endId = reader.nextString()
+                                                "labels" -> endLabels = reader.nextArray().map {a -> a.toString()}
+                                                "properties" -> endProperties = reader.nextObject()
+                                                else -> println("Miss")
+                                            }
+                                        }
+                                        end = Node(endId, endLabels, endProperties)
+                                    }
+                                    else -> println("Miss")
+                                }
+                            }
+                            var nr : NodeOrRelationship =
+                            when (type) {
+                                "node" -> GraphNode(id.toInt(), properties as Map<String, Any>, labels)
+                                "relationship" -> Relationship(id.toInt(), properties as Map<String, Any>, label, start!!.id.toInt(), end!!.id.toInt())
+                                else -> throw NoSuchElementException("Only Nodes and Relationships accepted")
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
+
+data class Node(
+    var id: String,
+    var labels: List<String>,
+    var properties: JsonObject?
+)
+
+open class NodeOrRelationship(open val id: Int, open val properties: Map<String, Any>)
+
+data class GraphNode(override val id: Int, override val properties: Map<String, Any>, val labels: List<String>) : NodeOrRelationship(id, properties)
+
+data class Relationship(override val id: Int, override val properties: Map<String, Any>, val label: String, val startId: Int, val endId: Int) : NodeOrRelationship(id, properties)
