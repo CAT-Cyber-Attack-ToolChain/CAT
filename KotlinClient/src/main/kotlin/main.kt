bdfef5e5c1c804eadbf0e5c3083a774977199d4e
phantomcorn <jj.phantakorn@outlook.com>
2022-10-27 19:53:17 +0100

    refactor: translate and integrate graph_gen_neo4j.py to Neo4JController

--- KotlinClient/src/main/kotlin/main.kt
+++ KotlinClient/src/main/kotlin/main.kt
@@ -1,3 +1,4 @@
+
 import contoller.MulvalController
 import contoller.Neo4JController
 import model.AttackGraphOutput
@@ -7,28 +8,10 @@ import org.neo4j.driver.Driver
 import org.neo4j.driver.GraphDatabase
 import org.neo4j.driver.Session
 import org.neo4j.driver.Values.parameters
-
 import metrics.decision.ShortestPath
 
 open class Main {
     companion object {
-        val driver: Driver = GraphDatabase.driver(
-            "neo4j+s://42ce3f9a.databases.neo4j.io",
-            AuthTokens.basic("neo4j", "qufvn4LK6AiPaRBIWDLPRzFh4wqzgI5x_n2bXHc1d38")
-        )
-
-        fun close() {
-            driver.close()
-        }
-
-        fun get_graph() {
-            val session: Session = driver.session()
-            val result: List<String> = session.writeTransaction { tx ->
-                val result: org.neo4j.driver.Result = tx.run("MATCH(n) RETURN n", parameters())
-                result.list { r -> r.toString() }
-            }
-            println(result)
-        }
 
         @JvmStatic
         fun main(args: Array<String>) {
@@ -43,6 +26,8 @@ open class Main {
             if (mulvalController.getGenerated()) {
                 neo4JController.update()
             }
+
+
         }
 
 //        fun generateGraphFromDatalog(inputFile : String, workingDirPath : String) {
