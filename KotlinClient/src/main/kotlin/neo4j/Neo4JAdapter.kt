3dcb1420c625174899a1bd14f54449ace8e3d3aa
Louis Heal <lh420@ic.ac.uk>
2022-10-28 13:11:55 +0100

    feat: create adapter class for neo4j and rewrite random attack agent to use it

---
+++ KotlinClient/src/main/kotlin/neo4j/Neo4JAdapter.kt
@@ -0,0 +1,32 @@
+package neo4j
+
+import org.neo4j.driver.*
+import org.neo4j.driver.Values.parameters
+
+class Neo4JAdapter {
+
+    val driver: Driver = GraphDatabase.driver(
+        "neo4j+s://42ce3f9a.databases.neo4j.io",
+        AuthTokens.basic("neo4j", "qufvn4LK6AiPaRBIWDLPRzFh4wqzgI5x_n2bXHc1d38")
+    )
+
+    fun getStartNode(): Int {
+        val session: Session = driver.session()
+        return session.writeTransaction { tx ->
+            val result: org.neo4j.driver.Result = tx.run(
+                "MATCH(n) WHERE n.text STARTS WITH \"attackerLocated\" RETURN n.node_id", Values.parameters()
+            )
+            result.list()[0].get(0).toString().toInt()
+        }
+    }
+
+    fun getConnectedNodes(id: Int): List<Int> {
+        val session: Session = driver.session()
+        val connectedNodeIds: List<Int> = session.writeTransaction { tx ->
+            val result: org.neo4j.driver.Result =
+                tx.run("MATCH(start {node_id: ${id}})-[:To]->(end) RETURN (end.node_id)", parameters())
+            result.list().map { e -> e[0].toString().toInt() }
+        }
+        return connectedNodeIds
+    }
+}
