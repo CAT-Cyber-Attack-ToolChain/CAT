bae391f64c96c90db72709cf60275c0eca31c89a
phantomcorn <jj.phantakorn@outlook.com>
2022-10-31 18:31:13 +0000

    refactor : removed FSKotlinClient folder + edit .gitignore

--- FSKotlinClient/src/jsMain/kotlin/Main.kt
+++
@@ -1,8 +0,0 @@
-import kotlinx.browser.document
-import react.create
-import react.dom.client.createRoot
-
-fun main() {
-    val container = document.getElementById("root") ?: error("Couldn't find container!")
-    createRoot(container).render(App.create())
-}
