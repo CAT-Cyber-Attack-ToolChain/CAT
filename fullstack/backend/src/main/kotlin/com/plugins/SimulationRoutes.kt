8487951fac27bd16523fff9c212d1dca6499ae77
phantomcorn <jj.phantakorn@outlook.com>
2022-11-09 15:51:55 +0000

    refactor : rename simulation routing + add to configureRouting

--- fullstack/backend/src/main/kotlin/com/plugins/SimulationRoutes.kt
+++ fullstack/backend/src/main/kotlin/com/plugins/SimulationRoutes.kt
@@ -6,7 +6,7 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Route.SimulationRoutes() {
+fun Route.SimulationRouting() {
 
     route("/simulation/random") {
         get {
