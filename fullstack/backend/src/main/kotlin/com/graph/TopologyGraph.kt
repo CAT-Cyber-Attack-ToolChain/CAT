a90c4ef82b8baed366d20280f0809d5490423ef8
Charles Loveman <cel20@ic.ac.uk>
2022-11-11 10:43:33 +0000

    Feat: Produce Graph representation from TopologyGraph

--- fullstack/backend/src/main/kotlin/com/graph/TopologyGraph.kt
+++ fullstack/backend/src/main/kotlin/com/graph/TopologyGraph.kt
@@ -2,6 +2,7 @@ package com.graph
 
 import java.io.BufferedReader
 import java.io.FileReader
+import javax.crypto.Mac
 
 data class Hacl(val m1: Machine, val m2: Machine, val protocol: String, val port : String) {
     override fun toString(): String {
@@ -21,37 +22,88 @@ data class NetworkServiceInfo(val machine: Machine, val application: Application
     }
 }
 
-class Machine(val name: String) {
+class Machine : GraphNode{
     val haclList: MutableList<Hacl> = mutableListOf()
     val accountList: MutableList<HasAccount> = mutableListOf()
     val services: MutableList<NetworkServiceInfo> = mutableListOf()
+    val name: String
+    val id: Int
+    var subnet: String = ""
+    constructor(_name: String, _id: Int) {
+        name = _name
+        id = _id
+    }
+    constructor(name: String) : this(name, next++)
+
+    override fun id(): Int = id
+
+    companion object {
+        var next : Int = 0
+    }
 
     override fun toString(): String {
         return name
     }
 
     fun build(): String {
-        return haclList.joinToString("\n") + "\n" + accountList.joinToString("\n") + "\n" + services.joinToString("\n")
-    }
-}
-
-class Subnet(val name: String) {
-    val machines: MutableSet<Machine> = mutableSetOf()
-
-    override fun toString(): String {
-        return machines.joinToString("\n") { x -> "inSubnet($x, $name)" }
+        val sb = StringBuilder()
+        var flag = false
+        if (haclList.isNotEmpty()) {
+            sb.append(haclList.joinToString("\n") {x -> x.toString()})
+            sb.append("\n")
+            flag = true
+        }
+        if (accountList.isNotEmpty()) {
+            sb.append(accountList.joinToString("\n"))
+            sb.append("\n")
+            flag = true
+        }
+        if (services.isNotEmpty()) {
+            sb.append(services.joinToString("\n"))
+            sb.append("\n")
+            flag = true
+        }
+        if (subnet != "") {
+            sb.append("inSubnet($name, $subnet)\n")
+            flag = true
+        }
+        if (flag) {
+            sb.append("\n")
+        }
+        return sb.toString()
     }
 }
 
 class Application(val name: String) {
+    val programInfo : MutableList<SetuidProgramInfo> = mutableListOf()
+    var client: Boolean = false
     override fun toString(): String {
         return name
+    }
+    fun build(): String {
+        val sb = StringBuilder()
+        if (client) {
+            sb.append("clientApplication($name)\n")
+        }
+        for (p in programInfo) {
+            sb.append(sb)
+        }
+        return sb.toString()
     }
 }
 
 class User(val name: String) {
+    var inCompetent = false
     override fun toString(): String {
         return name
+    }
+    fun build(): String {
+        val sb = StringBuilder()
+        if (inCompetent) {
+            sb.append("inCompetent($name)\n")
+        }
+
+        return sb.toString()
     }
 }
 
@@ -66,19 +118,19 @@ class Vulnerability(val name: String) {
     }
 
     fun build(): String {
-        return "vulProperty($name, $locality, $type).\n" + exists.joinToString("\n")
+        return "vulProperty($name, $locality, $type).\ncvss($name, $cvss).\n" + exists.joinToString("") + "\n"
     }
 }
 
 data class SetuidProgramInfo(val machine: Machine, val application: Application, val privilege: String) {
     override fun toString(): String {
-        return "setuidProgramInfo($machine, $application, $privilege)."
+        return "setuidProgramInfo($machine, $application, $privilege).\n"
     }
 }
 
 data class VulExists(val machine: Machine, val vulnerability: Vulnerability, val application: Application) {
     override fun toString(): String {
-        return "vulExists($machine, $vulnerability, $application)"
+        return "vulExists($machine, $vulnerability, $application).\n"
     }
 }
 
@@ -89,116 +141,139 @@ enum class CVSS {
     HIGH
 }
 
-class TopologyGraph(filepath: String) {
-    init {
-        val machines = mutableMapOf<String, Machine>()
-        val subnets = mutableMapOf<String, Subnet>()
-        val incompetents = mutableListOf<Machine>()
-        val clientApplications = mutableListOf<Application>()
-        val applications = mutableMapOf<String, Application>()
-        val setuidProgramInfo = mutableListOf<SetuidProgramInfo>()
-        val vulnerabilities = mutableMapOf<String, Vulnerability>()
-        val users = mutableMapOf<String, User>()
-        val reader = BufferedReader(FileReader(filepath))
-        for (line in reader.lines()) {
-            if (line == "") {
-                continue
-            }
-            val props = line.replace('(', ',').substring(0, line.length - 2).split(",")
-            when(props[0]) {
-                "hacl" -> {
-                    val m1 = getMachine(props[1], machines)
-                    val m2 = getMachine(props[2], machines)
-                    m1.haclList.add(Hacl(m1, m2, props[3], props[4]))
+class TopologyGraph(
+    nodes: MutableMap<Int, Machine>,
+    arcs: MutableMap<Int, MutableSet<Int>>,
+    val machines: MutableMap<String, Machine>,
+    val applications: MutableMap<String,Application>,
+    val vulnerabilities: MutableMap<String, Vulnerability>,
+    val users: MutableMap<String, User>
+) : Graph<Machine>(nodes, arcs) {
+    companion object {
+        fun build(filepath: String): TopologyGraph {
+            val machines = mutableMapOf<String, Machine>()
+            val applications = mutableMapOf<String, Application>()
+            val vulnerabilities = mutableMapOf<String, Vulnerability>()
+            val users = mutableMapOf<String, User>()
+            val reader = BufferedReader(FileReader(filepath))
+            for (line in reader.lines()) {
+                if (line == "") {
+                    continue
                 }
-                "inSubnet" -> {
-                    val m1 = getMachine(props[1], machines)
-                    val subnet = getSubnet(props[2], subnets)
-                    subnet.machines.add(m1)
-                }
-                "inCompetent" -> {
-                    val m = getMachine(props[1], machines)
-                    incompetents.add(m)
-                }
-                "vulExists" -> {
-                    val m = getMachine(props[1], machines)
-                    val v = getVulnerability(props[2], vulnerabilities)
-                    val a = getApplication(props[3], applications)
-                    v.exists.add(VulExists(m, v, a))
-                }
-                "vulProperty" -> {
-                    val v = getVulnerability(props[1], vulnerabilities)
-                    v.locality = props[2]
-                    v.type = props[3]
-                }
-                "isClient" -> {
-                    val app = getApplication(props[1], applications)
-                    clientApplications.add(app)
-                }
-                "setuidProgramInfo" -> {
-                    val m = getMachine(props[1], machines)
-                    val a = getApplication(props[2], applications)
-                    val upi = SetuidProgramInfo(m, a, props[3])
-                    setuidProgramInfo.add(upi)
-                }
-                "cvss" -> {
-                    val v = getVulnerability(props[1], vulnerabilities)
-                    when (props[2]) {
-                        "l" -> {v.cvss = CVSS.LOW}
-                        "m" -> {v.cvss = CVSS.MEDIUM}
-                        "h" -> {v.cvss = CVSS.HIGH}
+                val props = line.replace(Regex("\\s"), "").replace('(', ',').dropLast(2).split(",")
+                when (props[0]) {
+                    "hacl" -> {
+                        val m1 = getMachine(props[1], machines)
+                        val m2 = getMachine(props[2], machines)
+                        m1.haclList.add(Hacl(m1, m2, props[3], props[4]))
                     }
+                    "inSubnet" -> {
+                        val m1 = getMachine(props[1], machines)
+                        m1.subnet = props[2]
+                    }
+                    "inCompetent" -> {
+                        val u = getUser(props[1], users)
+                        u.inCompetent = true
+                    }
+                    "vulExists" -> {
+                        val m = getMachine(props[1], machines)
+                        val v = getVulnerability(props[2], vulnerabilities)
+                        val a = getApplication(props[3], applications)
+                        v.exists.add(VulExists(m, v, a))
+                    }
+                    "vulProperty" -> {
+                        val v = getVulnerability(props[1], vulnerabilities)
+                        v.locality = props[2]
+                        v.type = props[3]
+                    }
+                    "isClient" -> {
+                        val app = getApplication(props[1], applications)
+                        app.client = true
+                    }
+                    "setuidProgramInfo" -> {
+                        val m = getMachine(props[1], machines)
+                        val a = getApplication(props[2], applications)
+                        a.programInfo.add(SetuidProgramInfo(m, a, props[3]))
+                    }
+                    "cvss" -> {
+                        val v = getVulnerability(props[1], vulnerabilities)
+                        when (props[2]) {
+                            "l" -> {
+                                v.cvss = CVSS.LOW
+                            }
+                            "m" -> {
+                                v.cvss = CVSS.MEDIUM
+                            }
+                            "h" -> {
+                                v.cvss = CVSS.HIGH
+                            }
+                        }
+                    }
+                    "hasAccount" -> {
+                        val u = getUser(props[1], users)
+                        val m = getMachine(props[2], machines)
+                        m.accountList.add(HasAccount(u, m, props[3]))
+                    }
+                    "networkServiceInfo" -> {
+                        val m = getMachine(props[1], machines)
+                        val a = getApplication(props[2], applications)
+                        m.services.add(NetworkServiceInfo(m, a, props[3], props[4], props[5]))
+                    }
+                    else -> continue
                 }
-                "hasAccount" -> {
-                    val u = getUser(props[1], users)
-                    val m = getMachine(props[2], machines)
-                    m.accountList.add(HasAccount(u, m, props[3]))
-                }
-                "networkServiceInfo" -> {
-                    val m = getMachine(props[1], machines)
-                    val a = getApplication(props[2], applications)
-                    m.services.add(NetworkServiceInfo(m, a, props[3], props[4], props[5]))
-                }
-
-                else -> continue
             }
+            val sb = StringBuilder()
+            for (m in machines.values) {
+                sb.append(m.build())
+            }
+            for (v in vulnerabilities.values) {
+                sb.append(v.build())
+            }
+            for (u in users.values) {
+                sb.append(u.build())
+            }
+            for (a in applications.values) {
+                sb.append(a.build())
+            }
+            println(sb)
+            val nodes = mutableMapOf<Int, Machine>()
+            val arcs = mutableMapOf<Int, MutableSet<Int>>()
+            for (m in machines.values) {
+                nodes[m.id()] = m
+                arcs[m.id()] = mutableSetOf()
+                for (h in m.haclList) {
+                    arcs[m.id()]!!.add(h.m2.id())
+                }
+            }
+            return TopologyGraph(nodes, arcs, machines, applications, vulnerabilities, users)
         }
-        machines.values.map { x -> println(x.build()) }
-        vulnerabilities.values.map { x -> println(x.build()) }
-    }
 
-    fun getMachine(name: String, machines: MutableMap<String, Machine>): Machine {
-        if (!machines.containsKey(name)) {
-            machines[name] = Machine(name)
+        private fun getMachine(name: String, machines: MutableMap<String, Machine>): Machine {
+            if (!machines.containsKey(name)) {
+                machines[name] = Machine(name)
+            }
+            return machines[name]!!
         }
-        return machines[name]!!
-    }
 
-    fun getSubnet(name: String, subnets: MutableMap<String, Subnet>): Subnet {
-        if (!subnets.containsKey(name)) {
-            subnets[name] = Subnet(name)
+        private fun getApplication(name: String, map: MutableMap<String, Application>): Application {
+            if (!map.containsKey(name)) {
+                map[name] = Application(name)
+            }
+            return map[name]!!
         }
-        return subnets[name]!!
-    }
 
-    fun getApplication(name: String, map: MutableMap<String, Application>): Application {
-        if (!map.containsKey(name)) {
-            map[name] = Application(name)
+        private fun getVulnerability(name: String, map: MutableMap<String, Vulnerability>): Vulnerability {
+            if (!map.containsKey(name)) {
+                map[name] = Vulnerability(name)
+            }
+            return map[name]!!
         }
-        return map[name]!!
-    }
 
-    fun getVulnerability(name: String, map: MutableMap<String, Vulnerability>): Vulnerability {
-        if (!map.containsKey(name)) {
-            map[name] = Vulnerability(name)
+        private fun getUser(name: String, map: MutableMap<String, User>): User {
+            if (!map.containsKey(name)) {
+                map[name] = User(name)
+            }
+            return map[name]!!
         }
-        return map[name]!!
-    }
-
-    fun getUser(name: String, map: MutableMap<String, User>): User {
-        if (!map.containsKey(name)) {
-            map[name] = User(name)
-        }
-        return map[name]!!
     }
 }
