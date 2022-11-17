db2a4b0e65220cbd5eeb2b2aa3255de02e225a1e
phantomcorn <jj.phantakorn@outlook.com>
2022-11-17 13:35:46 +0000

    feat : color all duplicate edge during simulation

--- fullstack/frontend/src/components/Cytoscape.js
+++ fullstack/frontend/src/components/Cytoscape.js
@@ -94,7 +94,8 @@ var stylesheet = [
             targetArrowColor: '#000',
             arrowScale : 5,
             targetArrowShape: 'triangle',
-            curveStyle : 'taxi'
+            curveStyle : 'taxi',
+            //'control-point-step-size' : '1000'
         }
     },
     {   selector : '.attackedNode',
@@ -160,21 +161,27 @@ const Cytoscape = ({graph}) => {
     /*
         Find id of edge on graph with corresponding src and dst
         Returns id of nodes and edges the belongs on the graph
+
+        Note: edges is an array of array of edgeIds [[edgeId]] since two nodes can be traversed from two
     */
     function simulationParser(attackedPath) {
+
         const nodes = getNodesFromPath(attackedPath)
         const edges = []
-        
+      
         for (var i = 0; i < attackedPath.length; i++) {
             const src = attackedPath[i].first
             const dst = attackedPath[i].second
             const queryPath = JSON.parse(graph).filter((item)=> (item.data.source === src) && (item.data.target === dst))
             if (queryPath.length !== 0) {
-                edges.push(queryPath[0].data.id)
+                edges.push(queryPath.map((path) => (
+                    path.data.id
+                )))
             } else {
                 console.error('Could not find edges with source : ' + src + ' target: ' + dst)
             }
         }
+        
         return {nodes: nodes, edges: edges}
     }
 
@@ -209,7 +216,7 @@ const Cytoscape = ({graph}) => {
 
         function highlightEdge(index) {
           // animate the path (if not the last node)
-          cyRef.$('#' + attacked.edges[index]).addClass("attackedEdge")
+          attacked.edges[index].forEach(edgeId => cyRef.$('#' + edgeId).addClass("attackedEdge"))
           setTimeout(function(){highlightNode(index + 1)}, 500)
         }
 
