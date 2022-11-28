21fe24ec60e304f1c06b839d3ad5b93e88279159
ad4220 <anand.doshi20@imperial.ac.uk>
2022-11-28 11:17:52 +0000

    Merging stashed changes.

--- fullstack/frontend/src/components/ConfigurableAttackAgent.js
+++ fullstack/frontend/src/components/ConfigurableAttackAgent.js
@@ -1,18 +1,35 @@
 import { useState } from 'react';
+import axios from "axios";
 
 function ConfigurableAttackAgentForm() {
-  const [techniqueMap, setTechniqueMap] = useState([])
+
+  const host = process.env.REACT_APP_HOST
+  const port = process.env.REACT_APP_PORT
+
+
+  const [techniqueMap, setTechniqueMap] = useState(new Map())
   const [formElements, setFormElements] = useState([])
   const [count, setCount] = useState(0)
 
   function generateFormField() {
-
+    var techniqueScore = {
+      "technique": null,
+      "score": 0
+    }
     return (
-      <label>Enter techinque name:
-        <input
+      <label style={{ display: "flex", justifyContent: "space-evenly" }}>
+        <input className="input-width"
           type="text"
           value={techniqueMap[count]}
-          onChange={(e) => setTechniqueMap(prevState => [...prevState, e.target.value])}
+          onChange={(e) => techniqueScore.technique = e.target.value}
+        />
+        <input className="input-width"
+          type="number"
+          value={techniqueMap[count]}
+          onChange={((e) => setTechniqueMap(prevState => {
+            techniqueScore.score = e.target.value
+            return prevState.set(techniqueScore.technique, techniqueScore.score)
+          }))}
         />
       </label>
     )
@@ -23,18 +40,35 @@ function ConfigurableAttackAgentForm() {
     setFormElements(prevState => [...prevState, generateFormField()])
   }
 
+  async function sendTechniquesToBackend() {
+    try {
+      const response = await axios.post(`http://${host}:${port}/attack/custom`, {
+        techniqueMap: JSON.stringify(Array.from(techniqueMap.entries()))
+      })
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
   return (
-    <>
-    <form>
-      {generateFormField()}
+    <div style={{display: "flex", flexDirection:"column"}}>
+      <form style={{ display: "flex", flexDirection: "column"}}>
+        <label style={{ display: "flex", justifyContent: "space-evenly" }}>
+          <label className='input-width'>Technique Name</label>
+          <label className='input-width'>Score</label>
+        </label>
+        
+        {generateFormField()}
 
-      {
-        formElements.map((item) => <>{item}</>)
-      }
-    </form>
-
-     <button id="cut-button" style={{position: "absolute", zIndex: 1, left: 0, bottom: 0,  margin : "0 0 20px 20px"}} onClick={() => addFormField()}> Add new technique </button>
-    </>
+        {
+          formElements.map((item) => <>{item}</>)
+        }
+      </form>
+      <div style={{display: "flex", justifyContent: "space-evenly"}}>
+        <button id="add-button" onClick={() => addFormField()}> Add new technique </button>
+        <button id="submit-button" onClick={() => sendTechniquesToBackend()}> Submit </button>
+      </div>
+    </div>
   )
 }
 
