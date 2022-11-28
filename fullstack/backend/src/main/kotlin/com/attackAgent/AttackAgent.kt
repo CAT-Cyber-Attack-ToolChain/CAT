514334f197dad46d1b62a8d7edc5635cc133d747
Louis Aldous <louis.aldous20@imperial.ac.uk>
2022-11-28 10:38:15 +0000

    refactor: made fields in Neo4JAdapter private and added getters

--- fullstack/backend/src/main/kotlin/com/attackAgent/AttackAgent.kt
+++ fullstack/backend/src/main/kotlin/com/attackAgent/AttackAgent.kt
@@ -6,45 +6,45 @@ import com.neo4j.Rule
 
 abstract class AttackAgent {
 
-    protected val path: MutableList<RuleNodePair> = mutableListOf()
-    protected val adapter : Neo4JAdapter = Neo4JAdapter()
+  private val path: MutableList<RuleNodePair> = mutableListOf()
+  private val adapter: Neo4JAdapter = Neo4JAdapter()
 
-    fun attack() {
-        val startNode = adapter.getGraph()
-        var currNode = startNode
+  fun attack() {
+    val startNode = adapter.getGraph()
+    var currNode = startNode
 
-        while (currNode.connections.isNotEmpty()) {
-            var rule = chooseRule(currNode)
-            path.add(RuleNodePair(currNode, rule))
-            currNode = rule.dest
-        }
+    while (currNode.getConnections().isNotEmpty()) {
+      var rule = chooseRule(currNode)
+      path.add(RuleNodePair(currNode, rule))
+      currNode = rule.getDest()
+    }
+  }
+
+  protected abstract fun chooseRule(n: Node): Rule
+
+  fun printPath() {
+    for (pair in path) {
+      println(pair.rule.getText())
+      println(pair.node.getPermission())
+    }
+  }
+
+
+  //Converts path to a pairs of string
+  fun returnPath(): MutableList<Pair<String, String>> {
+    val paths = mutableListOf<Pair<String, String>>()
+
+    for (ruleNodePair: RuleNodePair in path) {
+
+      paths.add(Pair("n${ruleNodePair.node.getId()}", "n${ruleNodePair.rule.getDest().getId()}"))
     }
 
-    protected abstract fun chooseRule(n: Node): Rule
-
-    fun printPath() {
-        for (pair in path) {
-            println(pair.rule.rule)
-            println(pair.node.permission)
-        }
-    }
-
-
-    //Converts path to a pairs of string
-    fun returnPath(): MutableList<Pair<String,String>> {
-        val paths = mutableListOf<Pair<String,String>>()
-
-        for (ruleNodePair: RuleNodePair in path) {
-
-            paths.add(Pair("n${ruleNodePair.node.id}", "n${ruleNodePair.rule.dest.id}"))
-        }
-
-        return paths
-    }
+    return paths
+  }
 
 }
 
 class RuleNodePair(
-    val node: Node,
-    val rule: Rule
+        val node: Node,
+        val rule: Rule
 ) {}
