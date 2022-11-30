275ff80e08c0a69fa63ced78f841259e6af4f140
Louis Aldous <louis.aldous20@imperial.ac.uk>
2022-11-30 15:31:28 +0000

    fix: incorrect import in ApplicationTest

--- fullstack/backend/src/test/kotlin/com/example/ApplicationTest.kt
+++ fullstack/backend/src/test/kotlin/com/example/ApplicationTest.kt
@@ -5,7 +5,7 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import kotlin.test.*
 import io.ktor.server.testing.*
-import com.example.plugins.*
+import com.plugins.configureRouting
 
 class ApplicationTest {
     @Test
