edb0bb016cac0d6a9706e5f606f5c6840f9ce97c
Tao Wang <tw520@imperial.ac.uk>
2023-01-05 19:57:48 +0000

    feat: initial design for metrics ui

--- fullstack/frontend/src/components/Metrics.js
+++ fullstack/frontend/src/components/Metrics.js
@@ -1,3 +1,6 @@
+import './Metrics.css';
+import React from 'react';
+
 const metricsContainerStyle = {
   width : "100%",
   backgroundColor: "#0a111f",
@@ -7,26 +10,76 @@ const metricsContainerStyle = {
   flexDirection: "column",
 } 
 
+// const Metrics = ({mets}) => {
+
+//     return (
+//         <div style={metricsContainerStyle}>
+//           <h4>Metrics</h4>
+//           {mets == null
+//             ? <p>No metrics to calculate, upload a graph</p>
+//             : <div style={{overflow : "scroll"}}>
+//               <div>shortest path: {mets["shortestpath"]}</div>
+//               <div>mean path length: {mets["meanpathlength"]}</div>
+//               <div>normalised mean of path lengths: {mets["normalisedmopl"]}</div>
+//               <div>mode of path lengths: {mets["modepathlength"]}</div>
+//               <div>sd of path lengths: {mets["sdpathlength"]}</div>
+//               <div>number of paths: {mets["numberofpaths"]}</div>
+//               <div>weakest adversary: {mets["weakestadversary"]}</div>
+//             </div>
+//           }
+//         </div>
+//     )
+// }
+
+const mets2 = {shortestpath : "1", meanpathlength: "2", normalisedmopl: "3", modepathlength: "4", sdpathlength: "5", numberofpaths: "6", weakestadversary: "7"}
 
 const Metrics = ({mets}) => {
 
-    return (
-        <div style={metricsContainerStyle}>
-          <h4>Metrics</h4>
-          {mets == null
-            ? <p>No metrics to calculate, upload a graph</p>
-            : <div style={{overflow : "scroll"}}>
-              <div>shortest path: {mets["shortestpath"]}</div>
-              <div>mean path length: {mets["meanpathlength"]}</div>
-              <div>normalised mean of path lengths: {mets["normalisedmopl"]}</div>
-              <div>mode of path lengths: {mets["modepathlength"]}</div>
-              <div>sd of path lengths: {mets["sdpathlength"]}</div>
-              <div>number of paths: {mets["numberofpaths"]}</div>
-              <div>weakest adversary: {mets["weakestadversary"]}</div>
+  return (
+      <div style={metricsContainerStyle}>
+        <h4>Metrics</h4>
+        {mets == null
+          ? <div className="App">
+            <div className="column1">
+              <div className="decision">Shortest Path
+                <p className='text'>{mets2["shortestpath"]}</p>  
+              </div>
+              <div className='decision'>Number of Paths
+                <p className='text'>{mets2["numberofpaths"]}</p>
+              </div> 
+              <div className='decision'>Weakest Adversary
+                <p className='text'>{mets2["weakestadversary"]}</p> 
+              </div>
             </div>
-          }
-        </div>
-    )
+              
+            <div className="column2">
+              <div className='decision'>Mean Path Length
+                <p className='text'>{mets2["meanpathlength"]}</p> 
+              </div> 
+              <div className='decision'>Normalised Mean of Path Lengths
+                <p className='text'>{mets2["normalisedmopl"]}</p> 
+              </div> 
+              <div className='decision'>SD of Path Lengths 
+                <p className='text'>{mets2["sdpathlength"]}</p>
+              </div> 
+            </div>
+              
+            <div className="column3">
+              <div className='decision'>Mode of Path Lengths
+                <p className='text'>{mets2["modepathlength"]}</p> 
+              </div> 
+              <div className='decision'>Median of Path Lengths
+                <p className='text'>{mets2["sdpathlength"]}</p> 
+              </div> 
+              <div className='filler'>you will never see this
+                <p>you will never see this</p>
+              </div>
+            </div>
+          </div>
+          : <p>Nondbsmsdbmsd metrics to calculate, upload a graph</p>
+        }
+      </div>
+  )
 }
 
 export default Metrics;
