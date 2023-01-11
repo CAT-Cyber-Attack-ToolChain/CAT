da960963e8206fca244251188afbed3ced1c43e6
Purin Sukpanichnant <ps1620@ic.ac.uk>
2023-01-11 17:56:48 +0000

    styled light mode

--- fullstack/frontend/src/components/Metrics.js
+++ fullstack/frontend/src/components/Metrics.js
@@ -1,40 +1,10 @@
 import './Metrics.css';
 import React from 'react';
 
-const metricsContainerStyle = {
-  width : "100%",
-  backgroundColor: "#0a111f",
-  padding: "20px",
-  height: "200px",
-  display : "flex",
-  flexDirection: "column",
-} 
-
-// const Metrics = ({mets}) => {
-
-//     return (
-//         <div style={metricsContainerStyle}>
-//           <h4>Metrics</h4>
-//           {mets == null
-//             ? <p>No metrics to calculate, upload a graph</p>
-//             : <div style={{overflow : "scroll"}}>
-//               <div>shortest path: {mets["shortestpath"]}</div>
-//               <div>mean path length: {mets["meanpathlength"]}</div>
-//               <div>normalised mean of path lengths: {mets["normalisedmopl"]}</div>
-//               <div>mode of path lengths: {mets["modepathlength"]}</div>
-//               <div>sd of path lengths: {mets["sdpathlength"]}</div>
-//               <div>number of paths: {mets["numberofpaths"]}</div>
-//               <div>weakest adversary: {mets["weakestadversary"]}</div>
-//             </div>
-//           }
-//         </div>
-//     )
-// }
-
 const Metrics = ({mets}) => {
 
   return (
-      <div style={metricsContainerStyle}>
+      <div className="metrics-container">
         <h4>Metrics</h4>
         {mets == null
           ? 
